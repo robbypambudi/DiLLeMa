c5059847bad330eb560aff12c56202a8e6d93739
Robby Pambudi <robby.pambudi10@gmail.com>
2025-03-15 14:42:55 +0700

    fix: fixing error when running client

--- dillema/ray_cluster/client_node.py
+++ dillema/ray_cluster/client_node.py
@@ -1,20 +1,22 @@
+import argparse
 import ray
 import time
 import logging
 
-def start_ray_worker(data: dict):
+def start_ray_worker(args: argparse.ArgumentParser):
     """
     Start Ray worker node and connect to an existing head node.
     """
     logging.basicConfig(level=logging.INFO)
+    address = f"{args.head_host}:{args.head_port}"
 
     try:
         # Ray initialization for worker node
         ray.init(
             ignore_reinit_error=True,  # Ignore error if Ray is already initialized
-            address=f"{data.head_node_ip}:{data.port}",  # Connect to head node using IP and port
+            address=address
         )
-        logging.info(f"Ray worker node connected to head node at {data.head_node_ip}:{data.port}")
+        logging.info(f"Ray worker node connected to head node at {args.head_host}:{args.head_port}")
         
         while True:
             time.sleep(60)
